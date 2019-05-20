34ee8dda7555d19bf8b1caacb7c553054cdc5ccc
marieauxille denis <marieauxille@gmail.com>
2019-05-20 17:12:36 +0200

    without optional

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -20,6 +20,22 @@ class FlatsController < ApplicationController
     end
   end
 
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
   private
 
   def flat_params
